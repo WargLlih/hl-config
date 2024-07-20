18f99d73b32590f9d455da4ec94ee4255b7a93ad
Daniel Jr <daniel.souza.lima@outlook.com>
2024-07-19 22:37:23 -0300

    updated kitty and wl .sh

--- wl-pkgs-setup.sh
+++ wl-pkgs-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Sy git neovim wl-clipboard zsh polkit-gnome waybar adobe-source-han-sans-cn-fonts filelight zsh-autosuggestions zsh-syntax-highlighting ripgrep unzip starship swappy grim slurp ttf-jetbrains-mono-nerd noto-fonts-emoji wofi pamixer mako brightnessctl lazygit go btop sof-firmware
+sudo pacman -Sy git neovim wl-clipboard zsh polkit-gnome waybar adobe-source-han-sans-cn-fonts filelight zsh-autosuggestions zsh-syntax-highlighting ripgrep unzip starship swappy grim slurp ttf-jetbrains-mono-nerd noto-fonts-emoji wofi pamixer mako brightnessctl lazygit go btop sof-firmware hyprpaper
 
 yay -S bluetuith swaylock-effects spotify
 
